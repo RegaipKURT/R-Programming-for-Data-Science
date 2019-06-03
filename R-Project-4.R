a123beeea895daf8e91623b9403dbf2c14a80f09
Regaip KURT <regkrt@gmail.com>
2019-06-04 01:49:08 +0300

    R Project Ortamın Kaydedilmesi ve Scriptler

---
+++ R-Project-4.R
@@ -0,0 +1,18 @@
+# R Project Ortamın Kaydedilmesi ve Scriptler
+
+x = c(1,2,3,4,5,6) #liste oluşturma
+x = seq(1:6) #1'den 6'ya kadar olan sayılar için bu şekilde de oluşturabiliriz.
+getwd() # şu anki çalışma dizinini gösterir
+
+setwd("/home/pars") # çalışma dizinini yeniden istediğimiz bir yere belirtebiliriz.
+
+save.image("kayit.rdata") #şu anki çalışma içeriğimizi bir dosyaya kaydettik.
+
+load("kayit.rdata") # kaydettiğimiz dosyayı çalışma ortamına yükeldik.
+
+load(file.choose()) #burada açılan pencereden de yüklemek istediğimiz dosyayı seçebiliriz.
+
+ls # dosyayı yükledikten sonra oluşturduğumuz x burada görünecek.
+#denemek için programı açıp kapatın ve değişkenlerin yüklendiğini göreceksiniz.
+
+#remove(list = ls()) # oluşturduğumuz bütün değişkenleri hafızadan sildik
