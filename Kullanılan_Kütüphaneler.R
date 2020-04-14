fba3b61aa1557fa988ac784ec7272ed83207c3f7
Regaip KURT <regkrt@gmail.com>
2020-04-14 14:24:58 +0300

    Sık kullanılan kütüphanelerin yüklenmesi

---
+++ Kullanılan_Kütüphaneler.R
@@ -0,0 +1,90 @@
+"
+AŞAĞIDA BULUNAN KÜTÜPHANELERİ YORUM SATIRI OLMAKTAN ÇIKARIP 
+TEK SEFERDE YÜKLEYEBİLİRSİNİZ. UZUN SÜRECEKTİR AMA İLK KURULUMDA 
+YÜKLENMESİNDE FAYDA OLAN, İŞLERİ KOLAYLAŞTIRAN KÜTÜPHANELERDİR.
+
+HEMEN ALTINDA KÜTÜPHANELER YÜKLENEREK DENEMESİ YAPILABİLİR. 
+HATA ALINMASI DURUMUNDA HATA MESAJINDAKİ BİLGİLERE BAKARAK
+SİSTEMDE EKSİK PAKETLER VARSA KURULMALIDIR. 
+"
+
+# install.packages("ggplot2") # görselleştirme kütüphanesi
+# install.packages(ggpubr)
+# install.packages("tidyverse")
+# install.packages("mclust") # mclust kümeleme algoritması - büyük veri setlerinde kümeleme
+# install.packages("dplyr")
+# install.packages("ISLR")
+# install.packages("pls")#kismi en kucuk kareler ve pcr icin
+# install.packages("PerformanceAnalytics")
+# install.packages("funModeling")
+# install.packages("caret")
+# install.packages("pROC")
+# install.packages("class")
+# install.packages("e1071")#knn-svm gibi algoritmalar icin
+# install.packages("kernlab") #svm için
+# install.packages("ROCR") #roc için (roc curve gibi)
+# install.packages("neuralnet")
+# install.packages("GGally")
+# install.packages("nnet")
+# install.packages("rpart") #ağaç algoritmaları vb.
+# install.packages("rpart.plot")#rpart gorsel icin
+# install.packages(partykit) #karar agaci gorseli icin
+# install.packages("cli")
+# install.packages("tree")
+# install.packages("randomForest")
+# install.packages("gbm")
+# install.packages("xgboost")
+# install.packages("DiagrammeR")
+# install.packages("mlbench")
+# install.packages("broom") #tidy model icin
+# install.packages("elasticnet")
+# install.packages("glmnet")
+# install.packages("MASS")
+# install.packages("Matrix") 
+# install.packages("pgmm") #olive data seti icin 
+# install.packages("dslabs")
+# install.packages("ipred") #bagging icin 
+# install.packages("NeuralNetTools") #garson fonksiyonu icin
+# install.packages("FNN")
+# install.packages("AppliedPredictiveModeling")
+
+
+library(ggplot2)
+library(mclust)
+library(dplyr)
+library(pls) 
+library(PerformanceAnalytics)
+library(ISLR)
+library(tidyverse)
+library(funModeling)
+library(caret)
+library(pROC)
+library(class)
+library(e1071)
+library(kernlab) 
+library(ROCR)
+library(neuralnet)
+library(GGally)
+library(nnet)
+library(rpart)
+library(cli)
+library(tree)
+library(rpart.plot)
+library(gbm)
+library(xgboost)
+library(DiagrammeR)
+library(mlbench)
+library(AppliedPredictiveModeling)
+library(elasticnet)
+library(broom)
+library(glmnet)
+library(MASS)
+library(Matrix) 
+library(pgmm)
+library(dslabs)
+library(partykit)
+library(ipred) 
+library(randomForest)
+library(NeuralNetTools) 
+library(FNN)
+library(ggpubr)
